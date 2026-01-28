d5246922d7540c8e99334f55a24297d02b401cd3
maikhoac3vinhhoa-sudo <maikhoa.c3vinhhoa@gmail.com>
2026-01-28 18:38:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
-    // Removed specific base path to allow deployment at root domain on Vercel
-    base: '/', 
     define: {
       'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY),
     }
