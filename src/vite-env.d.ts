eaed15ef56c22b1c899e765cb437f679a46bafea
maikhoac3vinhhoa-sudo <maikhoa.c3vinhhoa@gmail.com>
2026-01-28 11:47:38 +0700

    Add type declarations for external modules

---
+++ src/vite-env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="vite/client" />
+
+declare module 'xlsx' {
+  const content: any;
+  export = content;
+}
+
+declare module 'mammoth' {
+  const content: any;
+  export = content;
+}
+
+declare module 'pdfjs-dist' {
+  const content: any;
+  export = content;
+}
+
+declare module 'pdfjs-dist/build/pdf.worker.min.js' {
+  const content: any;
+  export = content;
+}
